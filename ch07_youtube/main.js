035662066d7d2466d6cc5c997568d4e0d5347505
chunkind <chunkind@naver.com>
2022-01-22 01:28:19 +0900

    no message

--- ch07_youtube/main.js
+++ ch07_youtube/main.js
@@ -2,6 +2,6 @@ const moreBtn = document.querySelector('.info .metadata .titleAndButton .moreBtn
 const title = document.querySelector('.info .metadata .titleAndButton .title');
 
 moreBtn.addEventListener('click', ()=>{
-    moreBtn.classList.toggle('clicked');
-    title.classList.toggle('clamp');
+  moreBtn.classList.toggle('clicked');
+  title.classList.toggle('clamp');
 });
